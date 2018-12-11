d8ec8a9a93f2eea7b9a3a86643cd9b91e769ea5c
Umberto <Umbertogotti1979@gmail.com>
2018-12-11 14:24:18 +0100

    NewEnryViewModel introduced and tested.

---
+++ TripLog/TripLog/ViewModels/NewEntryViewModel.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TripLog.Models;
+using Xamarin.Forms;
+
+namespace TripLog.ViewModels
+{
+    public class NewEntryViewModel : BaseViewModel
+    {
+        #region Observables
+
+        private string _title;
+        public string Title
+        {
+            get { return _title; }
+            set
+            {
+                _title = value;
+                OnPropertyChanged();
+                SaveCommand.ChangeCanExecute();
+            }
+        }
+
+        private double _latitude;
+        public double Latitude
+        {
+            get { return _latitude; }
+            set { _latitude = value; OnPropertyChanged(); }
+        }
+
+        private double _longitude;
+        public double Longitude
+        {
+            get { return _longitude; }
+            set { _longitude = value; OnPropertyChanged(); }
+        }
+
+        private DateTime _date;
+        public DateTime Date
+        {
+            get { return _date; }
+            set { _date = value; OnPropertyChanged(); }
+        }
+
+        private int _rating;
+        public int Rating
+        {
+            get { return _rating; }
+            set { _rating = value; OnPropertyChanged(); }
+        }
+
+        private string _notes;
+        public string Notes
+        {
+            get { return _notes; }
+            set { _notes = value; OnPropertyChanged(); }
+        }
+
+        #endregion
+
+        #region Commands
+        private Command _saveCommand;
+        public Command SaveCommand
+        {
+            get
+                
+            {
+                if (_saveCommand == null)
+                {
+                    _saveCommand = new Command(ExecuteSaveCommand, CanSave);
+                }
+                return _saveCommand;
+            }
+        }
+
+        private bool CanSave(object arg)
+        {
+            return !string.IsNullOrEmpty(Title);
+        }
+
+        private void ExecuteSaveCommand(object obj)
+        {
+            var newEntry = new TripLogEntry
+            {
+                Title = Title,
+                Notes = Notes,
+                Rating = Rating,
+                Date = Date,
+                Latitude = Latitude,
+                Longitude = Longitude
+            };
+
+            // Save this new entry later: Introduce a data layer.
+        }
+
+        #endregion
+
+
+        public NewEntryViewModel()
+        {
+            Date = DateTime.Today;
+            Rating = 1;
+        }
+    }
+}
--- TripLog/TripLog/Views/NewEntryPage.xaml.cs
+++ TripLog/TripLog/Views/NewEntryPage.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using TripLog.ViewModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +15,8 @@ namespace TripLog.Views
 		public NewEntryPage ()
 		{
 			InitializeComponent ();
+
+            BindingContext = new NewEntryViewModel();
 		}
 
         private void Save(object sender, EventArgs e)
