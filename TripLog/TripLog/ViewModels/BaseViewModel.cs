39b911f89cb25759a99954878a58da310c323bbb
Umberto <Umbertogotti1979@gmail.com>
2018-12-11 13:44:18 +0100

    MainViewModel implemented and integrated.

---
+++ TripLog/TripLog/ViewModels/BaseViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace TripLog.ViewModels
+{
+    public abstract class BaseViewModel : INotifyPropertyChanged
+    {
+        protected BaseViewModel()
+        {
+
+        }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
--- TripLog/TripLog/Views/MainPage.xaml.cs
+++ TripLog/TripLog/Views/MainPage.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TripLog.Models;
+using TripLog.ViewModels;
 using Xamarin.Forms;
 
 namespace TripLog.Views
@@ -14,40 +15,7 @@ namespace TripLog.Views
         {
             InitializeComponent();
 
-            // Add hard-coded entries here
-
-            var items = new List<TripLogEntry>
-            {
-                new TripLogEntry
-                {
-                    Title = "Washington Monument",
-                    Notes = "Amazing!",
-                    Rating = 3,
-                    Date = new DateTime(2017, 2, 5),
-                    Latitude = 38.8895,
-                    Longitude = -77.0352
-                },
-                new TripLogEntry
-                {
-                    Title = "Statue of Liberty",
-                    Notes = "Inspiring!",
-                    Rating = 4,
-                    Date = new DateTime(2017, 4, 13),
-                    Latitude = 40.6892,
-                    Longitude = -74.0444
-                },
-                new TripLogEntry
-                {
-                    Title = "Golden Gate Bridge",
-                    Notes = "Foggy, but beautiful.",
-                    Rating = 5,
-                    Date = new DateTime(2017, 4, 26),
-                    Latitude = 37.8268,
-                    Longitude = -122.4798
-                }
-            };
-
-            trips.ItemsSource = items;
+            BindingContext = new MainViewModel();
         }
 
         public void New_Clicked(object sender, EventArgs e)
